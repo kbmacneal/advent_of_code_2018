1d080c64aefc1963c92184ba1def596442aee44c
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-12-09 13:23:09 -0600

    getting the next few days set up

---
+++ day_eleven/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace day_eleven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
