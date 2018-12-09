45a8ab2bfb430d771732cdd8716864d31ead69da
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-12-09 09:46:07 -0600

    day j get!

---
+++ day_nine/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+class MainClass {
+    static int players = 471;
+    static int marbles = 72026* 100;
+
+    static long[] scores = new long[players];
+    static LinkedList<int> placed = new LinkedList<int> ();
+    static LinkedListNode<int> current = placed.AddFirst (0);
+
+    static void next () {
+        current = current.Next ?? placed.First;
+    }
+    static void previous () {
+        current = current.Previous ?? placed.Last;
+    }
+
+    public static void Main (string[] args) {
+        for (int m = 0; m < marbles; ++m) {
+            if (((m + 1) % 23) == 0) {
+                previous ();
+                previous ();
+                previous ();
+                previous ();
+                previous ();
+                previous ();
+                previous ();
+
+                var j = m % players;
+                scores[j] += m + 1 + current.Value;
+
+                var tmp = current;
+                next ();
+                placed.Remove (tmp);
+            } else {
+                next ();
+                current = placed.AddAfter (current, m + 1);
+            }
+        }
+        Console.WriteLine (scores.Max ());
+    }
+}
---
+++ day_nine/README.md
@@ -0,0 +1,3 @@
+i was really deep into it when i looked at the list of answers, apparently doing double linked lists is the way to go lol. I'll be studying this one too.
+
+https://www.reddit.com/r/adventofcode/comments/a4i97s/2018_day_9_solutions/ebeudal/
