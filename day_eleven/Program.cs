e05628e6d15c99965f77a7d792b0cf5cdcaf1de8
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-12-11 13:43:33 -0600

    reformat and code cleanup

--- day_eleven/Program.cs
+++ day_eleven/Program.cs
@@ -13,7 +13,6 @@ namespace day_eleven
             public int x { get; set; }
             public int y { get; set; }
             public int power_level { get; set; }
-
             public int get_power_level(int input)
             {
                 int temp = (((this.x + 10) * this.y) + input) * (this.x + 10);
@@ -22,15 +21,13 @@ namespace day_eleven
 
                 return hundredDigit - 5;
             }
-
         }
 
         public class result
         {
             public fuel_cell top_left { get; set; }
             public int total_power { get; set; }
-            public int size {get;set;}
-
+            public int size { get; set; }
             public override string ToString()
             {
                 return JsonConvert.SerializeObject(this, Formatting.Indented);
@@ -39,7 +36,6 @@ namespace day_eleven
         static void Main(string[] args)
         {
             var input = 7347;
-            // var input = 18;
 
             List<fuel_cell> cells = new List<fuel_cell>();
 
@@ -68,14 +64,15 @@ namespace day_eleven
 
             foreach (var cell in cells)
             {
-                var temp = get_three_by_three_power_rating(cell,cells,3);
+                var temp = get_three_by_three_power_rating(cell, cells, 3);
 
-                if(temp > max_power)
+                if (temp > max_power)
                 {
-                    max_result = new result(){
+                    max_result = new result()
+                    {
                         top_left = cell,
                         total_power = temp,
-                        size=3
+                        size = 3
                     };
                     max_power = temp;
                 }
@@ -85,7 +82,6 @@ namespace day_eleven
 
             foreach (var cell in cells)
             {
-
                 for (int i = 3; i < 300; i++)
                 {
                     var part_2 = get_three_by_three_power_rating(cell, cells, i);
@@ -106,14 +102,9 @@ namespace day_eleven
                         max_power_part_two = part_2;
                     }
                 }
-
-
             }
-
-            
             //if this doesnt work, the answer is 1,1,300
             Console.WriteLine(max_part_two.ToString());
-
         }
 
         public static int get_three_by_three_power_rating(fuel_cell top_left, List<fuel_cell> grid, int search_size)
