06478e8eda10c24e91c433619147fc6198df302a
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-12-23 14:34:09 -0600

    switching to tuples

--- day_twenty_three/Program.cs
+++ day_twenty_three/Program.cs
@@ -11,15 +11,15 @@ namespace day_twenty_three {
             public int x { get; set; }
             public int y { get; set; }
             public int z { get; set; }
-
-            public int ManhattanDist (int x, int y, int z) {
-                return Math.Abs (this.x - x) + Math.Abs (this.y - y) + Math.Abs (this.z - z);
-            }
         }
         public class nanobot {
             public point point { get; set; }
             public int radius { get; set; }
             public bool in_range { get; set; } = false;
+
+            public int ManhattanDist (int x, int y, int z) {
+                return Math.Abs (this.point.x - x) + Math.Abs (this.point.y - y) + Math.Abs (this.point.z - z);
+            }
         }
         static void Main (string[] args) {
             var inputs = File.ReadAllLines ("input.txt");
@@ -54,70 +54,52 @@ namespace day_twenty_three {
             var max_bot = bots.First (e => e.radius == max_rad);
 
             foreach (var bot in bots) {
-                bot.in_range = bot.point.ManhattanDist (max_bot.point.x, max_bot.point.y, max_bot.point.z) <= max_rad;
+                bot.in_range = bot.ManhattanDist (max_bot.point.x, max_bot.point.y, max_bot.point.z) <= max_rad;
             }
 
             Console.WriteLine (bots.Where (e => e.in_range).Count ());
 
-            var max_x = bots.Max(e=>e.point.x);
-            var max_y = bots.Max(e=>e.point.y);
-            var max_z = bots.Max(e=>e.point.z);
+            var max_x = bots.Max (e => e.point.x);
+            var max_y = bots.Max (e => e.point.y);
+            var max_z = bots.Max (e => e.point.z);
 
-            List<point> points = new List<point>();
+            List<(int, int, int)> points = new List<(int,int,int)> ();
 
-            for (int x = 0; x < max_x; x++)
-            {
-                for (int y = 0; y < max_y; y++)
-                {
-                    for (int z = 0; z < max_z; z++)
-                    {
-                        points.Add(new point(){
-                            x = x,
-                            y=y,
-                            z=z
-                        });
+            for (int x = 0; x < max_x; x++) {
+                for (int y = 0; y < max_y; y++) {
+                    for (int z = 0; z < max_z; z++) {
+                        points.Add ((x,y,z));
                     }
                 }
             }
 
-            // foreach (var point in points)
-            // {
-            //     foreach (var bot in bots)
-            //     {
-            //         if(bot.ManhattanDist(point.x,point.y,point.z)<= bot.radius)
-            //         {
-            //             point.bots_in_range.Add(bot);
-            //         }
-            //     }
-            // }
-
-            var best_point = new point();
+            var best_point = (0,0,0);
 
             var count = 0;
 
+            foreach (var point in points) {
+                var temp = bots.Count (e => e.ManhattanDist (point.Item1, point.Item2, point.Item3) <= e.radius);
 
-            foreach (var point in points)
-            {
-                var temp = bots.Where(e=>e.point.ManhattanDist(point.x,point.y,point.z)<=e.radius).Count();
-
-                if(temp == count)
-                {
-                    if(point.ManhattanDist(0,0,0) < best_point.ManhattanDist(0,0,0))
-                    {
+                if (temp == count) {
+                    if (dist_from_0 (point.Item1, point.Item2, point.Item3) < dist_from_0 (best_point.Item1, best_point.Item2, best_point.Item3)) {
                         best_point = point;
                         count = temp;
                         continue;
                     }
                 }
 
-                if(temp > count)
-                {
+                if (temp > count) {
                     count = temp;
-                    best_point= point;
+                    best_point = point;
                 }
             }
 
-            Console.WriteLine(best_point.ManhattanDist(0,0,0));
+            Console.WriteLine (dist_from_0 (best_point.Item1, best_point.Item2, best_point.Item3));
+
+        }
+
+        public static int dist_from_0 (int x, int y, int z) {
+            return Math.Abs (x) + Math.Abs (y) + Math.Abs (z);
 
         }
 
