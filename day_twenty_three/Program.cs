aa8e8554f3f3b33a5cdf88f3449fa6b99e9ecd73
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-12-23 13:16:07 -0600

    yeesh

---
+++ day_twenty_three/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_twenty_three {
+    class Program {
+
+        public class point {
+            public int x { get; set; }
+            public int y { get; set; }
+            public int z { get; set; }
+
+            public int ManhattanDist (int x, int y, int z) {
+                return Math.Abs (this.x - x) + Math.Abs (this.y - y) + Math.Abs (this.z - z);
+            }
+        }
+        public class nanobot {
+            public point point { get; set; }
+            public int radius { get; set; }
+            public bool in_range { get; set; } = false;
+        }
+        static void Main (string[] args) {
+            var inputs = File.ReadAllLines ("input.txt");
+
+            List<nanobot> bots = new List<nanobot> ();
+
+            foreach (var line in inputs) {
+                var splits = line.Split (", ");
+
+                var coords = splits[0].Replace ("pos=<", "").Replace (">", "");
+
+                var x = coords.Split (",") [0];
+                var y = coords.Split (",") [1];
+                var z = coords.Split (",") [2];
+
+                var radius = splits[1].Replace ("r=", "");
+
+                nanobot bot = new nanobot () {
+                    point = new point () {
+                    x = Int32.Parse (x),
+                    y = Int32.Parse (y),
+                    z = Int32.Parse (z)
+                    },
+                    radius = Int32.Parse (radius)
+                };
+
+                bots.Add (bot);
+            }
+
+            var max_rad = bots.Max (e => e.radius);
+
+            var max_bot = bots.First (e => e.radius == max_rad);
+
+            foreach (var bot in bots) {
+                bot.in_range = bot.point.ManhattanDist (max_bot.point.x, max_bot.point.y, max_bot.point.z) <= max_rad;
+            }
+
+            Console.WriteLine (bots.Where (e => e.in_range).Count ());
+
+            var max_x = bots.Max(e=>e.point.x);
+            var max_y = bots.Max(e=>e.point.y);
+            var max_z = bots.Max(e=>e.point.z);
+
+            List<point> points = new List<point>();
+
+            for (int x = 0; x < max_x; x++)
+            {
+                for (int y = 0; y < max_y; y++)
+                {
+                    for (int z = 0; z < max_z; z++)
+                    {
+                        points.Add(new point(){
+                            x = x,
+                            y=y,
+                            z=z
+                        });
+                    }
+                }
+            }
+
+            // foreach (var point in points)
+            // {
+            //     foreach (var bot in bots)
+            //     {
+            //         if(bot.ManhattanDist(point.x,point.y,point.z)<= bot.radius)
+            //         {
+            //             point.bots_in_range.Add(bot);
+            //         }
+            //     }
+            // }
+
+            var best_point = new point();
+
+            var count = 0;
+
+
+            foreach (var point in points)
+            {
+                var temp = bots.Where(e=>e.point.ManhattanDist(point.x,point.y,point.z)<=e.radius).Count();
+
+                if(temp == count)
+                {
+                    if(point.ManhattanDist(0,0,0) < best_point.ManhattanDist(0,0,0))
+                    {
+                        best_point = point;
+                        count = temp;
+                        continue;
+                    }
+                }
+
+                if(temp > count)
+                {
+                    count = temp;
+                    best_point= point;
+                }
+            }
+
+            Console.WriteLine(best_point.ManhattanDist(0,0,0));
+
+        }
+
+    }
+}
---
+++ day_twenty_three/test.py
@@ -0,0 +1,76 @@
+import numpy as np
+import re
+
+with open('input.txt') as f:
+    data = f.read().splitlines()
+
+mins = np.zeros(3, dtype=np.int64)
+maxes = np.zeros(3, dtype=np.int64)
+
+points = np.zeros((1000, 3), dtype=np.int64)
+radii = np.zeros(1000, dtype=np.int64)
+
+re_str = '<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)'
+for line_i, line in enumerate(data):
+    x, y, z, r = [int(i) for i in re.search(re_str, line).groups()]
+    point = np.array([x, y, z])
+    maxes = np.maximum(maxes, point)
+    mins = np.minimum(mins, point)
+    points[line_i, :] = point
+    radii[line_i] = r
+
+min_val = np.min(mins)
+max_val = np.max(maxes)
+
+
+def compute_counts(points, points2, radii):
+    dists = np.sum(np.abs(np.expand_dims(points, 0) - np.expand_dims(points2, 1)), axis=2)
+    counts = np.sum(dists <= np.expand_dims(radii, 0), axis=1)
+
+    return counts
+
+best_point = np.mean(points, axis=0).astype(np.int64)
+best_count = 0
+
+t = 0
+mutation_rate = (max_val - min_val) // 10
+
+mutation = np.random.randint(-mutation_rate, mutation_rate+1, size=(100000, 3))
+points2 = np.expand_dims(best_point, 0) + mutation
+mutation_rate = 100000
+
+i = 0
+while True:
+    i += 1
+    if i % 500 == 0:
+        print(i, best_count, mutation_rate, ','.join([str(i) for i in best_point]))
+    counts = compute_counts(points, points2, radii)
+
+    highest_count = np.max(counts)
+    indices = np.arange(counts.size)
+    count_indices = indices[counts == highest_count]
+    matching_points = points2[count_indices, :]
+    mans = np.sum(np.abs(matching_points))
+    best_i = count_indices[np.argmin(mans)]
+
+    p = points2[best_i, :]
+    c = counts[best_i]
+    if c > best_count or c == best_count and np.sum(np.abs(p)) < np.sum(np.abs(best_point)):
+        best_count = c
+        best_point = np.copy(p)
+        t = 0
+    else:
+        t += 1
+
+    if t > 20000 and mutation_rate > 100:
+        mutation_rate //= 2
+        t = 0
+
+    if t > 100000:
+        break
+
+    mutation = np.random.randint(-mutation_rate, mutation_rate+1, size=(100, 3))
+    points2 = np.expand_dims(best_point, 0) + mutation
+
+print(','.join([str(i) for i in best_point]))
+print(np.sum(np.abs(best_point)))
