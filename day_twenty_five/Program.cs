bc1f1d39d4175b03bdbd3a65e28cd26e72b17445
Ken MacNeal <kenneth.b.macneal@gmail.com>
2018-12-25 16:27:44 -0600

    reformat

--- day_twenty_five/Program.cs
+++ day_twenty_five/Program.cs
@@ -16,60 +16,48 @@ namespace day_twenty_five {
         static void Main (string[] args) {
             var input = System.IO.File.ReadAllText ("input.txt");
 
-           var points = input.Split(new string[] { "\n", "," }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            var points = input.Split (new string[] { "\n", "," }, StringSplitOptions.RemoveEmptyEntries).Select (x => int.Parse (x)).ToArray ();
             var constellations = 0;
-            var spaceTimePoints = new List<point>();
-            for(var i =0; i < points.Count(); i +=4)
-            {
-                spaceTimePoints.Add(new point
-                {
+            var spaceTimePoints = new List<point> ();
+            for (var i = 0; i < points.Count (); i += 4) {
+                spaceTimePoints.Add (new point {
                     x = points[i],
-                    y = points[i + 1],
-                    z = points[i + 2],
-                    t = points[i + 3]
+                        y = points[i + 1],
+                        z = points[i + 2],
+                        t = points[i + 3]
                 });
             }
-            var allTried = new List<int>();
-            while (true)
-            {
-                var tried = new List<int>();
-                var pointsToCheck = new Queue<int>();
-                for (var i = 0; i < spaceTimePoints.Count(); i++)
-                {
-                    if (!allTried.Contains(i))
-                    {
-                        pointsToCheck.Enqueue(i);
+            var allTried = new List<int> ();
+            while (true) {
+                var tried = new List<int> ();
+                var pointsToCheck = new Queue<int> ();
+                for (var i = 0; i < spaceTimePoints.Count (); i++) {
+                    if (!allTried.Contains (i)) {
+                        pointsToCheck.Enqueue (i);
                         break;
                     }
                 }
-                while (pointsToCheck.Count > 0)
-                {
-                    var current = pointsToCheck.Dequeue();
-                    if(allTried.Contains(current))
-                    {
+                while (pointsToCheck.Count > 0) {
+                    var current = pointsToCheck.Dequeue ();
+                    if (allTried.Contains (current)) {
                         continue;
                     }
-                    tried.Add(current);
-                    allTried.Add(current);
-                    for (var i = 0; i < spaceTimePoints.Count(); i++)
-                    {
-                        if (!tried.Contains(i) && !allTried.Contains(i))
-                        {
-                            if ((ManhattanDist(spaceTimePoints[i], spaceTimePoints[current])) <= 3)
-                            {
-                                pointsToCheck.Enqueue(i);
+                    tried.Add (current);
+                    allTried.Add (current);
+                    for (var i = 0; i < spaceTimePoints.Count (); i++) {
+                        if (!tried.Contains (i) && !allTried.Contains (i)) {
+                            if ((ManhattanDist (spaceTimePoints[i], spaceTimePoints[current])) <= 3) {
+                                pointsToCheck.Enqueue (i);
                             }
                         }
                     }
                 }
-                if(tried.Count() == 0)
-                {
+                if (tried.Count () == 0) {
                     break;
                 }
                 constellations++;
             }
             // return constellations;
-        
 
             Console.WriteLine ("Part 1:");
             Console.WriteLine (constellations);
